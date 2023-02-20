07a8e6ef899c962359777a1271781733c95c040f
samelawton <samuellawton18@gmail.com>
2023-02-20 15:37:36 +0000

    404 error added as per comment

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const { getCategories } = require('./controllers/controller')
-const {error500Statuses} = require('./controllers/error-handling-controller')
+const {error500Statuses, error404Statuses} = require('./controllers/error-handling-controller')
 
 
 app.get('/api/categories', getCategories);
 
+app.use(error404Statuses);
 app.use(error500Statuses);
 
 module.exports = app;
--- controllers/error-handling-controller.js
+++ controllers/error-handling-controller.js
@@ -1,4 +1,8 @@
+
+exports.error404Statuses = (error, request, response, next) => {
+    response.status(404).send({msg: 'not found'})
+}
+
 exports.error500Statuses = (error, request, response, next) => {
-    console.log('The error is >>> ' + error);
     response.status(500).send({msg: 'server error'})
 }
